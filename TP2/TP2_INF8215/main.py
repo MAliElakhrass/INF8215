8dae84697a55229dac02f79b1f1acf318b61825a
Unknown <mohamedakhrass11@gmail.com>
2019-11-01 23:02:11 -0400

    Refactoring

--- TP2/TP2_INF8215/main.py
+++ TP2/TP2_INF8215/main.py
@@ -47,9 +47,8 @@ def testPossibleRockMoves():
 def test_print_move():
     rh = Rushhour([False], [2], [2], ["rouge"])
     s = State([0])
-    s = s.put_rock((3, 1)) # Roche dans la case 3-1
-    s = s.move(0, 1) # Voiture rouge vers la droite
-
+    s = s.put_rock((3, 1))  # Roche dans la case 3-1
+    s = s.move(0, 1)  # Voiture rouge vers la droite
     algo = MiniMaxSearch(rh, s, 1)
     algo.print_move(True, s)
     algo.print_move(False, s)
@@ -74,7 +73,6 @@ def test16moves():
     rh = Rushhour([True, True, False, False, True, True, False, False],
                   [2, 2, 3, 2, 3, 2, 3, 3],
                   [2, 0, 0, 0, 5, 4, 5, 3],
-     #                        1        2        3          4         5        6       7
                   ["rouge", "vert", "mauve", "orange", "emeraude", "lime", "jaune", "bleu"])
     s = State([1, 0, 1, 4, 2, 4, 0, 1])
     algo = MiniMaxSearch(rh, s, 1)
@@ -123,6 +121,18 @@ def test16moves3depth():
     algo.solve(s, False)
 
 
+def test14moves3depth():
+    rh = Rushhour([True, False, True, False, False, False, True, True, False, True, True],
+                  [2, 2, 3, 2, 2, 3, 3, 2, 2, 2, 2],
+                  [2, 0, 0, 3, 4, 5, 3, 5, 2, 5, 4],
+                  ["rouge", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+    s = State([0, 0, 3, 1, 2, 1, 0, 0, 4, 3, 4])
+    algo = MiniMaxSearch(rh, s, 3)
+    algo.rushhour.init_positions(s)
+    print(algo.rushhour.free_pos)
+    algo.solve(s, False)
+
+
 if __name__ == '__main__':
     """
     # print("Partie 1 Implémentation de Rush Hour (25pts)\n")
@@ -139,8 +149,12 @@ if __name__ == '__main__':
     print("Test 14 moves")
     test14moves()
     """
-
+    """
     print("2.2 Implémentation adversarielle")
+    print("Test 9 moves")
     test9moves3depth()
-
-
+    print("Test 16 moves")
+    test16moves3depth()
+    print("Test 14 moves")
+    test14moves3depth()
+    """
